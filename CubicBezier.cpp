1a7c7bae27d2521c7ce1bf1857efebc2eaed56d2
Michael Ploof <michael@dynamicperception.com>
2016-07-15 15:25:45 -0400

    Version for testing as C++ console application

--- CubicBezier.cpp
+++ CubicBezier.cpp
@@ -1,19 +1,53 @@
-# include "CubicBezier.h"
+#include "stdafx.h"
+#include "CubicBezier.h"
+
+
+#if defined(__AVR_AT90USB1287__)
+#define SERIAL USBSerial
+#define EMBEDDED
+#elif defined(__AVR_ATmega328P__) || defined(__AVR_ATmega328__)  || defined(__AVR_ATmega168__)
+#define SERIAL Serial
+#define EMBEDDED
+#else
+#define DESKTOP
+#endif
+
+#if defined(EMBEDDED)
+// Defines
+#define P SERIAL.print
+#define PLN SERIAL.println
+#define STRING String
+bool CubicBezier::g_embedded = true;
+#elif defined(DESKTOP)
+#include <iostream>
+#include <iomanip>
+bool CubicBezier::g_embedded = false;
+using std::cout;
+using std::endl;
+using std::hex;
+using std::dec;
+#endif
+
+
 
 /*
-
-	This library provides the ability to compute cubic bezier curves
-
+	This library provides the ability to compute cubic Bezier curves.
+	An optimized forward differencing method may be used when values
+	along the curve are desired in fixed t parameter increments.
 */
 
 /********** OrderedPair class functions **********/
 
 #pragma region OrderedPair Class Functions
+
+int OrderedPair::g_id_gen = 0;
+
 OrderedPair::OrderedPair(){}
 
 OrderedPair::OrderedPair(float p_x, float p_y){
-	val(0, p_x);			
-	val(1, p_y);
+	m_id = g_id_gen++;
+	m_vals[0] = p_x;
+	m_vals[1] = p_y;
 }
 
 float OrderedPair::val(int p_which){
@@ -22,7 +56,7 @@ float OrderedPair::val(int p_which){
 	return m_vals[p_which];
 }
 
-void OrderedPair::val(int p_which, float p_val){
+void OrderedPair::val(int p_which, float p_val){	
 	m_vals[p_which] = p_val;
 }
 
@@ -42,8 +76,23 @@ OrderedPair operator * (int p_x, const OrderedPair& p_op){
 OrderedPair operator * (const OrderedPair& p_op, int p_x){
 	return OrderedPair(p_op.m_vals[0] * p_x, p_op.m_vals[1] * p_x);
 }
+OrderedPair operator * (float p_x, const OrderedPair& p_op){
+	return OrderedPair(p_op.m_vals[0] * p_x, p_op.m_vals[1] * p_x);
+}
+OrderedPair operator * (const OrderedPair& p_op, float p_x){
+	return OrderedPair(p_op.m_vals[0] * p_x, p_op.m_vals[1] * p_x);
+}
 OrderedPair operator + (int p_x, const OrderedPair& p_op){
-	return OrderedPair(p_op.m_vals[0] + p_x, p_op.m_vals[1] * p_x);
+	return OrderedPair(p_op.m_vals[0] + p_x, p_op.m_vals[1] + p_x);
+}
+OrderedPair operator + (float p_x, const OrderedPair& p_op){
+	return OrderedPair(p_op.m_vals[0] + p_x, p_op.m_vals[1] + p_x);
+}
+OrderedPair operator + (const OrderedPair& p_op, int p_x){
+	return OrderedPair(p_op.m_vals[0] + p_x, p_op.m_vals[1] + p_x);
+}
+OrderedPair operator + (const OrderedPair& p_op, float p_x){
+	return OrderedPair(p_op.m_vals[0] + p_x, p_op.m_vals[1] + p_x);
 }
 OrderedPair operator + (const OrderedPair& p_op1, const OrderedPair& p_op2){
 	return OrderedPair(p_op1.m_vals[0] + p_op2.m_vals[0], p_op1.m_vals[1] + p_op2.m_vals[1]);
@@ -66,28 +115,27 @@ int Span::g_id_gen = 0;
 
 Span::Span(){}
 
-Span::Span(OrderedPair *p_ctrl_pts, Span *p_prev_span){	
+Span::Span(OrderedPair *p_ctrl_pts, Span *p_prev_span){		
 	m_id = g_id_gen++;
-	USBSerial.println("Span - Constructor");	
-	m_ctrl_pts = p_ctrl_pts;
+	m_h = NULL;
+	m_T = NULL;
+	m_t_steps_remain = NULL;
+	for (int i = 0; i < g_MAX_CTRL_PTS; i++){
+		m_ctrl_pts[i] = p_ctrl_pts[i];
+	}	
 	m_next_span = NULL;
-	prevSpan(p_prev_span);	
-	if(m_prev_span != NULL)
-		m_prev_span->nextSpan(this);	
-	setCoeffs();
-	USBSerial.println("Span - Done constructing");
+	prevSpan(p_prev_span);			
+	setCoeffs();	
 }
 
-void Span::setCoeffs(){
-	USBSerial.println("Span - Setting coeffs");
-	m_coeffs[0] = (-m_ctrl_pts[0]) + (3 * m_ctrl_pts[1]) + (-3 * m_ctrl_pts[2]) + m_ctrl_pts[3];
-	m_coeffs[1] = (3 * m_ctrl_pts[0]) + (-6 * m_ctrl_pts[1]) + (3 * m_ctrl_pts[2]);
-	m_coeffs[2] = (-3 * m_ctrl_pts[0]) + (3 * m_ctrl_pts[1]);
-	m_coeffs[3] = m_ctrl_pts[0];
+void Span::setCoeffs(){	
+	m_coeff_A = (-m_ctrl_pts[0]) + (3 * m_ctrl_pts[1]) + (-3 * m_ctrl_pts[2]) + m_ctrl_pts[3];
+	m_coeff_B = (3 * m_ctrl_pts[0]) + (-6 * m_ctrl_pts[1]) + (3 * m_ctrl_pts[2]);
+	m_coeff_C = (-3 * m_ctrl_pts[0]) + (3 * m_ctrl_pts[1]);
+	m_coeff_D = m_ctrl_pts[0];
 }
 
-void Span::nextSpan(Span *p_next_span){
-	USBSerial.println("Span - Setting next span");
+void Span::nextSpan(Span *p_next_span){	
 	m_next_span = p_next_span;
 }
 
@@ -95,9 +143,10 @@ Span* Span::nextSpan(){
 	return m_next_span;
 }
 
-void Span::prevSpan(Span *p_prev_span){
-	USBSerial.println("Span - Setting prev span");
+void Span::prevSpan(Span *p_prev_span){	
 	m_prev_span = p_prev_span;	
+	if (m_prev_span != NULL)
+		m_prev_span->nextSpan(this);
 }
 
 Span* Span::prevSpan(){
@@ -112,6 +161,7 @@ void Span::incrementSizeFromX(float p_x){
 	float h = p_x / rangeX();
 	incrementSize(h);
 	m_t_steps_remain = (int)round(1 / h);
+	//cout << "Span steps remaining: " << m_t_steps_remain << endl;
 }
 
 void Span::incrementSize(float p_h){
@@ -124,12 +174,22 @@ float Span::incrementSize(){
 }
 
 void Span::initForwardDiff(){
-	// Set the initial forward differences
-	USBSerial.println("Span - Initializing forward differences");
-	m_fdiff_vals[0] = m_coeffs[3];
-	m_fdiff_vals[1] = m_coeffs[0] * pow(m_h, 3);
-	m_fdiff_vals[2] = m_coeffs[1] * pow(m_h, 2);
-	m_fdiff_vals[3] = m_coeffs[2] * m_h;
+	// Set the initial forward differences	
+	m_fdiff_vals[0] = m_coeff_D;
+	m_fdiff_vals[1] = m_coeff_A * pow(m_h, 3) + m_coeff_B * pow(m_h, 2) + m_coeff_C * m_h;
+	m_fdiff_vals[3] = 6 * m_coeff_A * pow(m_h, 3);
+	m_fdiff_vals[2] = m_fdiff_vals[3] + (2 * m_coeff_B * pow(m_h, 2));	
+
+	/*cout << "h: " << m_h << "h^3: " << pow(m_h, 3) << "\n" << endl;
+
+	cout << "Coeffs:" << endl;
+	cout << "Ax: " << m_coeff_A.x() << " Bx: " << m_coeff_B.x() << " Cx: " << m_coeff_C.x() << " Dx: " << m_coeff_D.x() << endl;
+	cout << "Ay: " << m_coeff_A.y() << " By: " << m_coeff_B.y() << " Cy: " << m_coeff_C.y() << " Dy: " << m_coeff_D.y() << "\n" << endl;
+
+	cout << "Setting initial F-diffs" << endl;
+	cout << "Px: " << m_fdiff_vals[0].x() << " F1x: " << m_fdiff_vals[1].x() << " F2x: " << m_fdiff_vals[2].x() << " F3x: " << m_fdiff_vals[3].x() << endl;
+	cout << "Py: " << m_fdiff_vals[0].y() << " F1y: " << m_fdiff_vals[1].y() << " F2y: " << m_fdiff_vals[2].y() << " F3y: " << m_fdiff_vals[3].y() << "\n" << endl;
+	*/
 
 	// Zero the t step counter
 	m_T = 0;
@@ -140,56 +200,45 @@ int Span::stepsRemaining(){
 }
 	
 OrderedPair Span::positionAtNextT(){
+	//cout << "Px: " << m_fdiff_vals[0].x() << " F1x: " << m_fdiff_vals[1].x() << " F2x: " << m_fdiff_vals[2].x() << " F3x: " << m_fdiff_vals[3].x() << endl;
+	//cout << "Py: " << m_fdiff_vals[0].y() << " F1y: " << m_fdiff_vals[1].y() << " F2y: " << m_fdiff_vals[2].y() << " F3y: " << m_fdiff_vals[3].y() << "\n" << endl;
 	const int FORWARD_DIFFS = 3;
 	for (int i = 0; i < FORWARD_DIFFS; i++){
-		m_fdiff_vals[i] + m_fdiff_vals[i + 1];
+		m_fdiff_vals[i] = m_fdiff_vals[i] + m_fdiff_vals[i + 1];
 	}
 	m_T += m_h;
 	m_t_steps_remain--;
 	return m_fdiff_vals[0];
 }
 
-OrderedPair Span::positionAtT(float p_T){
-	USBSerial.print("Span - Getting position at t: ");
-	USBSerial.println(p_T);
+OrderedPair Span::positionAtT(float p_T){	
 	float pos[2];
 	for (int i = 0; i < 2; i++){
 		// B(t) = At^3 + Bt^2 + Ct + D
-		pos[i] = m_coeffs[0].val(i) * pow(p_T, 3) + m_coeffs[1].val(i) * pow(p_T, 2) + m_coeffs[2].val(i) * p_T + m_coeffs[3].val(i);
+		pos[i] = m_coeff_A.val(i) * pow(p_T, 3) + m_coeff_B.val(i) * pow(p_T, 2) + m_coeff_C.val(i) * p_T + m_coeff_D.val(i);
 	}
 	return OrderedPair(pos[0], pos[1]);
 }
 
-boolean Span::containsX(float p_x){
-	USBSerial.print("Spline contains x val ");
-	USBSerial.print(p_x);
-	USBSerial.print("? ");
-	if (p_x >= minX() && p_x <= maxX()){
-		USBSerial.println("True");
+bool Span::containsX(float p_x){	
+	if (p_x >= minX() && p_x <= maxX())	
 		return true;
-	}
-	else{
-		USBSerial.println("False");
+	else		
 		return false;
-	}
 }
 
-boolean Span::containsY(float p_y){
+bool Span::containsY(float p_y){
 	if (minY() <= p_y && p_y <= maxY())
 		return true;
 	else
 		return false;
 }
 
-float Span::minX(){	
-	USBSerial.print("minX: ");
-	USBSerial.println(m_ctrl_pts[0].x());
+float Span::minX(){		
 	return m_ctrl_pts[0].x();
 }
 
 float Span::maxX(){
-	USBSerial.print("maxX: ");
-	USBSerial.println(m_ctrl_pts[3].x());
 	return m_ctrl_pts[g_MAX_CTRL_PTS-1].x();
 }
 
@@ -209,16 +258,36 @@ float Span::rangeY(){
 	return maxY() - minY();
 }
 
+#if defined DESKTOP
 void Span::printCtrlPts(){
+	cout << "Span ID: " << id() << endl;
 	for(int i = 0; i < 4; i++){
-		USBSerial.print("ctrl_pt ");
-		USBSerial.print(i);
-		USBSerial.print(" x: ");
-		USBSerial.print(m_ctrl_pts[i].x());
-		USBSerial.print(" y: ");
-		USBSerial.println(m_ctrl_pts[i].y());
+		cout << "ctrl_pt " << i << " Address: " << hex << (int)&m_ctrl_pts[i] 
+			<< dec << " x: " << m_ctrl_pts[i].x() << " y: " << m_ctrl_pts[i].y() << endl;
 	}
-	USBSerial.println("Done printing points");
+	cout << "Done printing points" << endl;
+}
+#elif defined EMBEDDED
+void Span::printCtrlPts(){
+	P("Span ID: ");
+	PLN(id());
+	for (int i = 0; i < 4; i++){
+		P("ctrl_pt ");
+		P(i);
+		P(" Address: ");
+		P((int)&m_ctrl_pts[i], hex);
+		P(" x: ");
+		P(m_ctrl_pts[i].x());
+		P(" y: ");
+		PLN(m_ctrl_pts[i].y());
+	}
+	PLN("Done printing points");
+}
+#endif
+
+
+int Span::id(){
+	return m_id;
 }
 
 
@@ -236,22 +305,15 @@ CubicBezier::CubicBezier(){}
 	in a linear spline. Control points p1 and p2 for each span
 	may be modified later.
 */
-CubicBezier::CubicBezier(OrderedPair *p_ctrl_pts, int p_knot_count, boolean p_only_knots){
-	// Must have at least 2 knots to define a spline
-	USBSerial.println("Bez - Constructor");
+CubicBezier::CubicBezier(OrderedPair *p_ctrl_pts, int p_knot_count, bool p_only_knots){
+	// Must have at least 2 knots to define a spline	
 	if (p_knot_count < 2)
 		return;
 	m_ctrl_pts = p_ctrl_pts;	
 	m_knot_count = p_knot_count;
 	m_span_count = p_knot_count - 1;
 	m_mem_allocated = false;
-	m_only_knots = p_only_knots;
-	USBSerial.println("Bez - Pre-initalization");
-	m_spans[0].printCtrlPts();
-	initializeSpans();
-	USBSerial.println("Bez - Post-initalization");
-	m_spans[0].printCtrlPts();
-	USBSerial.println("Bez - Done constructing");
+	m_only_knots = p_only_knots;			
 }
 
 // Default destructor
@@ -264,22 +326,20 @@ void CubicBezier::releaseMemory(){
 		free(m_spans);
 }
 
-void CubicBezier::initializeSpans(){
-	USBSerial.println("Bez - Initializing spans");
+void CubicBezier::initSpans(){
+	
+	cout << "Bez - Initializing spans" << endl;
 	// Dynamically allocate memory for span array
-	if (m_mem_allocated){
-		USBSerial.println("Bez - Releasing allocated memory");
+	if (m_mem_allocated){	
+		cout << "Bez - Releasing allocated memory" << endl;
 		releaseMemory();
 	}
-	int span_size = sizeof(Span);
-	USBSerial.print("Bez - Span size: ");
-	USBSerial.println(span_size);
-	int memory_needed = m_span_count * span_size;
-	USBSerial.print("Bez - Spans: ");
-	USBSerial.println(m_span_count);
-	USBSerial.print("Bez - Memory needed: ");
-	USBSerial.println(memory_needed);
-	USBSerial.println("Bez - Allocating memory for span array");
+	size_t span_size = sizeof(Span);	
+	cout << "Bez - Span size: " << span_size << endl;	
+	size_t memory_needed = m_span_count * span_size;	
+	cout << "Bez - Spans: " << m_span_count << endl;
+	cout << "Bez - Memory needed: " << memory_needed << endl;
+	cout << "Bez - Allocating memory for span array" << endl;
 	m_spans = (Span *)malloc(memory_needed);
 	m_mem_allocated = true;
 
@@ -292,20 +352,14 @@ void CubicBezier::initializeSpans(){
 	*/
 	int adjust = m_only_knots ? 0 : 1;		
 	int PT_CT = 4;
-	for (int i = 0; i < m_span_count; i++){
-		USBSerial.println("Bez - Creating ctrl pt subset");
+	for (int i = 0; i < m_span_count; i++){				
 		OrderedPair ctrl_pt_subset[4] = { m_ctrl_pts[0 + i * PT_CT], m_ctrl_pts[0 + i * PT_CT + adjust], 
 			m_ctrl_pts[1 + i * PT_CT + adjust], m_ctrl_pts[1 + i * PT_CT + 2 * adjust] };
 		// Don't pass a garbage address to the pointer parameter for the first span
-		Span* prev_span = i == 0 ? NULL : &m_spans[i - 1];
-		USBSerial.println("Bez - Creating new span");
-		m_spans[i] = Span(ctrl_pt_subset, prev_span);
-	}
-	USBSerial.println("Bez - Setting current span pointer");
-	m_cur_span = &m_spans[0];
-	USBSerial.println("Bez - Span points");
-	printFirstSpanPts();
-	USBSerial.println("Bez - Existing span initalization");
+		Span* prev_span = i == 0 ? NULL : &m_spans[i - 1];				
+		m_spans[i] = Span(ctrl_pt_subset, prev_span);		
+	}		
+	m_cur_span = &m_spans[0];			
 }
 
 OrderedPair CubicBezier::positionAtT(float p_T){
@@ -321,21 +375,14 @@ OrderedPair CubicBezier::positionAtT(float p_T){
 
 	// Determine which span to check
 	for (int i = 0; i < m_span_count; i++){
-		USBSerial.println("Checking span with control pts of...");
-		m_spans[i].printCtrlPts();
-
 		if (m_spans[i].containsX(p_T)){
 			// Convert parameter to t of given span and get result
-			USBSerial.print("minX: ");
-			USBSerial.print(m_spans[i].minX());
-			USBSerial.print(" rangeX: ");
-			USBSerial.print(m_spans[i].rangeX());
-			USBSerial.print(" p_T: ");
-			USBSerial.println(p_T);
+			//cout << "minX: " << m_spans[i].minX() << " rangeX: " << m_spans[i].rangeX() << " p_T: " << p_T << "\n" << endl;
 			p_T = (p_T - m_spans[i].minX()) / m_spans[i].rangeX();
 			return m_spans[i].positionAtT(p_T);
 		}
 	}
+	return OrderedPair(0, 0);
 }
 
 void CubicBezier::printFirstSpanPts(){
@@ -353,21 +400,22 @@ OrderedPair CubicBezier::positionAtNextT(){
 		of the previous one.
 	*/
 	int end_count = 1;
-	if (m_cur_span->nextSpan() == NULL){
+	if (m_cur_span->nextSpan() == NULL){		
 		end_count = 0;
 	}
 
 	// Move on to the next span if necessary
-	if (m_cur_span->stepsRemaining() == end_count)
+	if (m_cur_span->stepsRemaining() < end_count){		
 		m_cur_span = m_cur_span->nextSpan();
+	}
 
 	m_t_steps_remain--;
 
 	return ret;
 }
 
-void CubicBezier::incrementSize(float p_h){	
-	m_t_steps_remain = 1 / p_h;
+void CubicBezier::initForwardDiff(float p_h){
+	m_t_steps_remain = (int)(1 / p_h);
 	p_h *= m_spans[m_span_count - 1].maxX();
 
 	// Set the increment sizes for each of the segments
--- CubicBezier.h
+++ CubicBezier.h
@@ -8,33 +8,47 @@
 #ifndef _BEZIER_h
 #define _BEZIER_h
 
+/*
 #if defined(ARDUINO) && ARDUINO >= 100
 	#include "Arduino.h"
 #else
 	#include "WProgram.h"
 #endif
+*/
 
 class OrderedPair {
+
 		public:
 			OrderedPair();
 			OrderedPair(float p_a, float p_b);
+			//~OrderedPair();
 			float val(int p_which);
 			void val(int p_which, float p_val);		
 			float x();
 			float y();			
 
+			int id();
+
 			friend OrderedPair operator *(int val, const OrderedPair &op);
 			friend OrderedPair operator *(const OrderedPair &op, int val);
+			friend OrderedPair operator *(float val, const OrderedPair &op);
+			friend OrderedPair operator *(const OrderedPair &op, float val);
 			friend OrderedPair operator +(int val, const OrderedPair &op);
+			friend OrderedPair operator +(const OrderedPair &op, int val);
+			friend OrderedPair operator +(float val, const OrderedPair &op);
+			friend OrderedPair operator +(const OrderedPair &op, float val);
 			friend OrderedPair operator + (const OrderedPair& p_op1, const OrderedPair& p_op2);
 			friend OrderedPair operator -(int val, const OrderedPair &op);
 			friend OrderedPair operator -(const OrderedPair &op);
 
 		private:
 			float m_vals[2];	// The (a, b) values
+			static int g_id_gen;
+			int m_id;
 };
 
 class Span {
+
 	public:
 		Span();
 		Span(OrderedPair *p_ctrl_pts, Span* p_prev_span);						
@@ -43,8 +57,8 @@ class Span {
 		void incrementSize(float p_h);
 		void incrementSizeFromX(float p_h);
 		float incrementSize();			
-		boolean containsX(float p_x);
-		boolean containsY(float p_y);			
+		bool containsX(float p_x);
+		bool containsY(float p_y);
 		float minX();
 		float maxX();
 		float rangeX();
@@ -59,6 +73,7 @@ class Span {
 		Span* nextSpan();
 		
 		void printCtrlPts();
+		int id();
 
 	private:			
 		void initForwardDiff();
@@ -67,8 +82,11 @@ class Span {
 		Span *m_next_span;
 		Span *m_prev_span;
 		static const int g_MAX_CTRL_PTS = 4;			
-		OrderedPair m_coeffs[g_MAX_CTRL_PTS];
-		OrderedPair *m_ctrl_pts;
+		OrderedPair m_coeff_A;
+		OrderedPair m_coeff_B;
+		OrderedPair m_coeff_C;
+		OrderedPair m_coeff_D;
+		OrderedPair m_ctrl_pts[g_MAX_CTRL_PTS];
 		float m_h;				// The t step size for forward differencing calculation
 		float m_T;
 		int m_t_steps_remain;
@@ -90,28 +108,32 @@ class CubicBezier
 		/********** Public Functions **********/
 
 		CubicBezier();
-		CubicBezier(OrderedPair *p_ctrl_pts, int p_knot_count, boolean p_only_knots);
+		CubicBezier(OrderedPair *p_ctrl_pts, int p_knot_count, bool p_only_knots);
 		~CubicBezier();
 		OrderedPair positionAtT(float p_T);
 		OrderedPair positionAtNextT();
-		void incrementSize(float p_h);
+		void initForwardDiff(float p_h);
 		float incrementSize();
 		int stepsRemaining();
 		void printFirstSpanPts();
+		void initSpans();
+
 	private:
 
 		/********** Private Variables **********/
 
-		boolean m_mem_allocated;
+		bool m_mem_allocated;
 		OrderedPair *m_ctrl_pts;
-		boolean m_only_knots;
-		void initializeSpans();
+		bool m_only_knots;
 		Span* m_cur_span;
 		float m_h;
 		float m_T;
 		int m_t_steps_remain;
+		static bool g_embedded;
 
 		/********** Private Functions **********/
+		
+		
 		void releaseMemory();
 };
 
